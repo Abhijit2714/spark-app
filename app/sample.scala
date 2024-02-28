1b9a2f4bfe9ae7953581b4dbdbae63535db2ba3b
Abhijit2714 <abijitdhamne@gmail.com>
2024-02-28 22:56:57 +0530

    Update sample.scala

--- app/sample.scala
+++ app/sample.scala
@@ -1,21 +1,25 @@
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
+// PiSpark.scala
+import org.apache.spark.sql.SparkSession
 
-object SparkPi {
-  def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Spark Pi")
-    val sc = new SparkContext(conf)
-    
-    val slices = if (args.length > 0) args(0).toInt else 2
-    val n = 100000 * slices
-    val count = sc.parallelize(1 to n, slices).map { i =>
-      val x = Math.random() * 2 - 1
-      val y = Math.random() * 2 - 1
-      if (x * x + y * y <= 1) 1 else 0
-    }.reduce(_ + _)
-    
-    println(s"Pi is roughly ${4.0 * count / n}")
-    
-    sc.stop()
+object PiSpark {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("Pi Calculation")
+      .master("local[*]")
+      .getOrCreate()
+
+    val numSamples = 1000000 // You can adjust this for more accuracy
+
+    val count = spark.sparkContext.parallelize(1 to numSamples)
+      .map { _ =>
+        val x = Math.random()
+        val y = Math.random()
+        if (x*x + y*y < 1) 1 else 0
+      }
+      .reduce(_ + _)
+
+    println(s"Pi is roughly ${4.0 * count / numSamples}")
+
+    spark.stop()
   }
 }
