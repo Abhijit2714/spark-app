7174269eddec049f769efef36edec92fad5ee11c
Abhijit2714 <abijitdhamne@gmail.com>
2024-03-12 15:17:25 +0530

    Update HelloWorldSpark.scala

--- app/HelloWorldSpark.scala
+++ app/HelloWorldSpark.scala
@@ -1,5 +1,5 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
-    println("Hello, It's 11th March...Have a nice Day!!!!!!!!!! Infivit Technologies, Nashik")
+    println("Hello, It's 11th March...Have a nice Day!!!!!!!!!! Infivit Technologies, Nashik1245678")
   }
 }
